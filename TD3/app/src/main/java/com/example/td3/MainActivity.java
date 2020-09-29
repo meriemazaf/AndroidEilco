ca72490da1eeb723fc497df4bd333416518f2fdd
meriem azaf <meriem.azaf.ma@gmail.com>
2020-09-29 15:14:20 +0200

    Ajout exo1

--- TD3/app/src/main/java/com/example/td3/MainActivity.java
+++ TD3/app/src/main/java/com/example/td3/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.td3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button rollButton = (Button) findViewById(R.id.btn);
+        final TextView txt = (TextView) findViewById(R.id.txt);
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast toast = Toast.makeText(MainActivity.this, "Dé lancé!", Toast.LENGTH_SHORT);
+                //toast.show();
+                int i = getRandomNumber(1,6);
+                txt.setText(String.valueOf(i));
+
+            }
+        });
+
+    }
+    public int getRandomNumber(int min,int max) {
+        return (new Random()).nextInt((max - min) + 1) + min;
     }
 }
